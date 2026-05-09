cecaa6c44031e899db30f8cee54677a8d36e35b9
Rafael San‑José <rsanjose@alxarafe.com>
2026-05-09 12:37:40 +0200

    Add test file to ensure phpcs doesn't fail

---
+++ php/tests/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Smoke;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function test_framework_is_operational(): void
+    {
+        $this->assertTrue(true);
+    }
+}
